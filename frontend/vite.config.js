a824a395cb3cd14de52cc67efe146125658c234b
Simon Belbeoch <simon.belbeoch@ln1.eu>
2025-09-27 12:14:39 +0200

    refactor: ajout de la taille des bundle

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    visualizer({
+      filename: 'dist/stats.html',
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
